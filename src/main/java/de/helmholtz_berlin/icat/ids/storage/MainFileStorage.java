24f6d84663744d9a1d543b3ee7ae6646a58d8b95
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2014-11-18 12:29:56 +0000

    Cleanup: remove not needed import.
    git-svn-id: http://manti.exp.bessy.de/svn/icat/ids-plugin-hzb@534 8efdbd46-c5fb-49ab-9956-99f62928ec21

--- src/main/java/de/helmholtz_berlin/icat/ids/storage/MainFileStorage.java
+++ src/main/java/de/helmholtz_berlin/icat/ids/storage/MainFileStorage.java
@@ -11,7 +11,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.UUID;
 
 import org.icatproject.ids.plugin.DsInfo;
 import org.icatproject.ids.plugin.MainStorageInterface;
