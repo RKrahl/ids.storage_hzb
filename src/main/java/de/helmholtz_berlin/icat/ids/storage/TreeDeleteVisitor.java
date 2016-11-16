5dd9c5658ca978b760a8dfc45fe94270726abf6e
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2016-11-16 14:01:54 +0100

    Remove obsolete code.

--- src/main/java/de/helmholtz_berlin/icat/ids/storage/TreeDeleteVisitor.java
+++ src/main/java/de/helmholtz_berlin/icat/ids/storage/TreeDeleteVisitor.java
@@ -12,12 +12,7 @@ public class TreeDeleteVisitor extends SimpleFileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) 
 	throws IOException {
-	// Make sure that a lock file, if present, is the very last
-	// thing that gets deleted in a directory.  Don't delete it
-	// here, but rather in postVisitDirectory().
-	if (! file.getFileName().toString().equals(".lock")) {
-	    Files.delete(file);
-	}
+	Files.delete(file);
 	return FileVisitResult.CONTINUE;
     }
 
@@ -25,11 +20,6 @@ public class TreeDeleteVisitor extends SimpleFileVisitor<Path> {
     public FileVisitResult postVisitDirectory(Path dir, IOException e) 
 	throws IOException {
 	if (e == null) {
-	    // Delete a lock file if present, see above.
-	    Path lockf = dir.resolve(".lock");
-	    if (Files.exists(lockf)) {
-		Files.delete(lockf);
-	    }
 	    Files.delete(dir);
 	    return FileVisitResult.CONTINUE;
 	} else {
