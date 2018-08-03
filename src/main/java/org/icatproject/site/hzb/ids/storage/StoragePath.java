09c932bf863a227ad2bed860d49557b3f12ac032
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2018-08-03 15:23:17 +0200

    Adapt storage path to new versions of proposal numbers.

--- src/main/java/org/icatproject/site/hzb/ids/storage/StoragePath.java
+++ src/main/java/org/icatproject/site/hzb/ids/storage/StoragePath.java
@@ -1,6 +1,9 @@
 package org.icatproject.site.hzb.ids.storage;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,29 +23,22 @@ public class StoragePath {
 
     // Number of name elements in the relative path down to dataset
     // level.  Must be in sync with the return value of getRelPath().
-    public static final int dsRelPathNameCount = 6;
+    public static final int dsRelPathNameCount = 5;
 
-    public static final Pattern invNameRegExp 
-	= Pattern.compile("(\\d{3})\\d{5}-[A-Z]+(?:/[A-Z]+)?");
-    public static final Pattern visitIdRegExp 
-	= Pattern.compile("\\d+\\.\\d+-[A-Z]+(?:/[A-Z]+)?");
-    public static final Pattern nameRegExp 
+    private static final Pattern nameSpaceRegExp
+	= Pattern.compile("([0-9a-z]+):([0-9A-Za-z][0-9A-Za-z~./_+-]+)");
+    private static final Map<String, Pattern> propNoRegExp
+	= createPropNoMap();
+    private static final Pattern nameRegExp
 	= Pattern.compile("[0-9A-Za-z][0-9A-Za-z~._+-]*");
 
-    private static String checkInvName(String invName) throws IOException {
-	Matcher m = invNameRegExp.matcher(invName);
-	if (!m.matches()) {
-	    throw new IOException("invalid invesigation name " + invName);
-	}
-	return m.group(1);
+    private static Map<String, Pattern> createPropNoMap() {
+	Map<String, Pattern> m = new HashMap<String, Pattern>();
+	m.put("gate1", Pattern.compile("(\\d{3})\\d{5}-[A-Z]+(?:/[A-Z]+)?-\\d+\\.\\d+-[A-Z]+(?:/[A-Z]+)?"));
+	m.put("gate2", Pattern.compile("[A-Z]+-(\\d{3})-\\d+-[A-Z]+(?:/[A-Z]+)?(?:-\\d+\\.\\d+)?"));
+	return Collections.unmodifiableMap(m);
     }
-
-    private static void checkVisitId(String visitId) throws IOException {
-	if (!visitIdRegExp.matcher(visitId).matches()) {
-	    throw new IOException("invalid visit id " + visitId);
-	}
-    }
-
+    
     public static void checkName(String name) throws IOException {
 	if (!nameRegExp.matcher(name).matches()) {
 	    throw new IOException("invalid name " + name);
@@ -50,17 +46,27 @@ public class StoragePath {
     }
 
     public static String getRelPath(DsInfo dsInfo) throws IOException {
-	String facilityName = dsInfo.getFacilityName();
 	String invName = dsInfo.getInvName();
-	String visitId = dsInfo.getVisitId();
 	String dsName = dsInfo.getDsName();
-	String cycle = checkInvName(invName);
-	checkVisitId(visitId);
+	Matcher im = nameSpaceRegExp.matcher(invName);
+        if (!im.matches()) {
+            throw new IOException("invalid invesigation name " + invName);
+        }
+	String nameSpace = im.group(1);
+	String propNo = im.group(2);
+	String cycle;
+	if (propNoRegExp.containsKey(nameSpace)) {
+	    Matcher pm = propNoRegExp.get(nameSpace).matcher(propNo);
+	    if (!pm.matches()) {
+		throw new IOException("invalid invesigation name " + invName);
+	    }
+	    cycle = pm.group(1);
+	} else {
+	    cycle = "000";
+	}
 	checkName(dsName);
-	checkName(facilityName);
-	return facilityName + "/" + cycle 
-	    + "/" + invName.replace('/', '_') 
-	    + "/" + visitId.replace('/', '_') 
+	return nameSpace + "/" + cycle 
+	    + "/" + propNo.replace('/', '_') 
 	    + "/data"
 	    + "/" + dsName;
     }
