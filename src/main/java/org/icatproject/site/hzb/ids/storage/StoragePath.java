4d76a0b81639ba74251d1bff3757746a472ffe98
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2021-10-29 11:22:40 +0200

    Fix regular pattern for misc proposal numbers

--- src/main/java/org/icatproject/site/hzb/ids/storage/StoragePath.java
+++ src/main/java/org/icatproject/site/hzb/ids/storage/StoragePath.java
@@ -31,7 +31,7 @@ public class StoragePath {
 	m.put("gate1", Pattern.compile("(\\d{3})\\d{5}-[A-Z]+(?:/[A-Z]+)?-\\d+\\.\\d+-[A-Z]+(?:/[A-Z]+)?"));
 	m.put("gate2", Pattern.compile("[A-Z]+-(\\d{3})-\\d+-[A-Z]+(?:/[A-Z]+)?(?:-\\d+\\.\\d+)?"));
 	m.put("pub", Pattern.compile("(\\d{2})-ND\\d{6}"));
-	m.put("misc", Pattern.compile("(\\d{2})\\d{6}"));
+	m.put("misc", Pattern.compile("(\\d{2})-\\d{6}"));
 	return Collections.unmodifiableMap(m);
     }
     
